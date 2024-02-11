71cec9308796ad334822b3efa6f7e5383144364f
Vahid Nejad <vahid884@gmail.com>
2024-02-11 08:02:21 +0330

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Get started by
+          editing&nbsp;
+          <code className="font-mono font-bold">
+            src/app/page.tsx
+          </code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -46,14 +49,21 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2
+            className={`mb-3 text-2xl font-semibold`}
+          >
             Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <p
+            className={`m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Find in-depth
+            information about
+            Next.js features
+            and API.
           </p>
         </a>
 
@@ -63,14 +73,21 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2
+            className={`mb-3 text-2xl font-semibold`}
+          >
             Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p
+            className={`m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Learn about
+            Next.js in an
+            interactive course
+            with&nbsp;quizzes!
           </p>
         </a>
 
@@ -80,14 +97,20 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2
+            className={`mb-3 text-2xl font-semibold`}
+          >
             Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p
+            className={`m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Explore starter
+            templates for
+            Next.js.
           </p>
         </a>
 
@@ -97,14 +120,21 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2
+            className={`mb-3 text-2xl font-semibold`}
+          >
             Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p
+            className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}
+          >
+            Instantly deploy
+            your Next.js site
+            to a shareable URL
+            with Vercel.
           </p>
         </a>
       </div>
